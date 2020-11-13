93acaabea085f8adb8c59d584beb53329b7bea84
jpg333 <joshpgriffith@gmail.com>
2020-11-13 18:03:52 -0500

    Work in progress

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+import os
+import pandas as pd
+import numpy as np
 import graphviz
 from sklearn import tree
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
-import pandas as pd
-import numpy as np
-
-file = "finalData.csv"
+from sklearn.metrics import *
+from sklearn.linear_model import LinearRegression
 
 
 # method for initializing .csv file
@@ -19,6 +20,123 @@ def initCSV(csvfile):
     pd.set_option('display.max_columns', 15)
 
     return df
+
+
+# method to select target column from dataset
+def targetCol(df):
+    print("Choose target attribute from available columns below: ")
+    # display columns
+    for i in range(len(df.columns)):
+        print(str(i+1) + ". " + df.columns[i])
+    # select target column
+    while True:
+        try:
+            ans = int(input("Enter the number of the target column above: "))
+            if ans not in range(1, len(df.columns)+1):
+                raise ValueError
+        except ValueError:
+            print("Error: please enter a number between 1 and " + str(len(df.columns)) + ".\n")
+        else:
+            break
+    return df.columns[ans]
+
+# method to select file from those already in the directory
+def existingFile():
+    # current directory
+    path_to_watch = "."
+
+    listing = []
+    for file in os.listdir(path_to_watch):
+        try:
+            pd.read_csv(file)
+            listing.append(file)
+        except (ValueError, PermissionError):
+            pass
+
+    for i in range(len(listing)):
+        print(str(i+1) + ". " + listing[i])
+
+    while True:
+        try:
+            ans = int(input("Enter the number of the file above that you'd like to use: "))
+            if ans not in range(1, len(listing)+1):
+                raise ValueError
+        except ValueError:
+            print("Error: please enter a number between 1 and " + str(len(listing)) + ".\n")
+        else:
+            break
+
+    return listing[ans-1]
+
+
+# method to monitor the current directory for added/removed files so that the user can add their own dataset
+def uploadFile():
+    # current directory
+    path_to_watch = "."
+
+    # get all files in directory
+    before = []
+    for file in os.listdir(path_to_watch):
+        before.append(file)
+
+    print("Add your .csv file to the current directory: " + os.getcwd())
+
+    # continue monitoring file uploads until one is chosen
+    chosen = False
+    while not chosen:
+        # list for updated listing
+        after = []
+        for file in os.listdir(path_to_watch):
+            after.append(file)
+
+        # if a file in after[] was not in before[] then it
+        for file in after:
+            if file not in before:
+                print("Added: " + file)
+                # send to confirm() to see if user wants this file. returns boolean that dictates if loop stops or not
+                chosen = confirm(file)
+                # if chosen, save file in final variable
+                if chosen:
+                    final = file
+                # else return to start
+                else:
+                    start()
+        for file in before:
+            if file not in after:
+                print("Removed: " + file)
+                before.remove(file)
+
+        # set new before listing for next iteration
+        before = after
+
+    # after loop, final file has been chosen
+    return final
+
+
+# method to confirm file choice and validity
+def confirm(f):
+
+    try:
+       pd.read_csv(f)
+    except (ValueError, PermissionError):
+        print("Error: chosen file is not a readable .csv. Please upload a different file.")
+        return False
+
+    print("Would you like to use the file " + f + " ?")
+    while True:
+        try:
+            ans = input("Enter 'y' or 'n': ")
+            if ans not in ('y', 'Y', 'n', 'N'):
+                raise ValueError
+        except ValueError:
+            print("Error: please enter 'y' for yes or 'n' for no.\n")
+        else:
+            break
+
+    if ans in ('y', 'Y'):
+        return True
+    elif ans in ('n', 'N'):
+        return False
 
 
 # function to drop a column in the list cols
@@ -142,79 +260,192 @@ def correlation(df):
     return drop(df, uniqueFlagged)
 
 
-data = initCSV(file)
-print("Original Dataset:")
-print(data)
-
-# newData = correlation(data)
-
-# X for attributes
-# Y for target value (label)
-attributes = data.drop('quality', axis=1)
-label = data['quality']
-qualities = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
-
-
-le = preprocessing.LabelEncoder()
-
-# X for attributes
-# Y for target value (label)
-attributes = data.drop('Action', axis=1)
-label = data['Action']
-code = le.fit(['allow', 'deny', 'drop', 'reset-both'])
-qualities = le.transform(['allow', 'deny', 'drop', 'reset-both'])
-
-# print(data.describe())
-
-
 # function to split data into training and testing for tree. test size is 1/5 of the data
-def split(x, y):
-    xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.20)
+def split(df, col):
+    # drop target label to define attributes
+    attributes = df.drop(col, axis=1)
 
-    return xtrain, xtest, ytrain, ytest
+    # df of target column
+    targets = df[col]
+
+    return attributes, targets
 
 
 # function to make a classifier decision tree from data
-def treeClassify(xtrain, xtest, ytrain, ytest, plot):
-    clf = tree.DecisionTreeClassifier()
-    clf.fit(xtrain, ytrain)
-    ypred = clf.predict(xtest)
+def treeClassify(x, y, file, plot):
 
+    # split x and y into train and test groups
+    xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.20)
+
+    # start classifier tree
+    clf = tree.DecisionTreeClassifier()
+    # teach tree with the training set
+    clf.fit(xtrain, ytrain)
+    # predict y based on testing x after training
+    ypred = clf.predict(xtest)
 
     # if plot is true, export as visualized tree
     if plot:
-        dot_data = tree.export_graphviz(clf, out_file=None, feature_names=attributes.columns, class_names=qualities, filled=True)
+        dot_data = tree.export_graphviz(clf, out_file=None, feature_names=x.columns, class_names=y, filled=True)
         graph = graphviz.Source(dot_data)
-        graph.render(file + "_Classifier")
+        graph.render(file.replace('.csv', '') + "_Classifier")
         print("Exported classifier tree to pdf")
-    # return prediction accuracy of algorithm
-    return pd.DataFrame({'Actual': ytest, 'Predicted': ypred})
+    # return prediction accuracy of algorithm (inverse transformed back to string target values) and percent accuracy
+    return pd.DataFrame({'Actual': (ytest), 'Predicted': (ypred)}), accuracy_score(ytest, ypred)*100
 
 
 # function to make a regression decision tree from data
-def treeRegression(xtrain, xtest, ytrain, ytest, plot):
+def treeRegression(x, y, file, plot):
+    # convert target label into integers for evaluation
+    # ['allow' 'deny' 'drop' 'reset-both'] mapped to [0, 1, 2, 3]
+    le = preprocessing.LabelEncoder()
+
+    le.fit(y)
+
+    try:
+        # string representation of labels
+        float(y[0])
+    except ValueError:
+        # integer representation of labels
+        y = le.transform(y)
+
+    # split x and y into train and test groups
+    xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.20)
+
     reg = tree.DecisionTreeRegressor()      # initialize regression tree
     reg = reg.fit(xtrain, ytrain)                 # fit
     ypred = reg.predict(xtest)
 
     # if plot is true, export as visualized tree
     if plot:
-        dot_data = tree.export_graphviz(reg, out_file=None, feature_names=attributes.columns, class_names=qualities, filled=True)
+        dot_data = tree.export_graphviz(reg, out_file=None, feature_names=x.columns, class_names=y, filled=True)
         graph = graphviz.Source(dot_data)
-        graph.render(file + "_Regression")
+        graph.render(file.replace('.csv', '') + "_Regression")
         print("Exported regression tree to pdf")
-    # return prediction accuracy of algorithm
-    return pd.DataFrame({'Actual': ytest, 'Predicted': ypred})
+    # return prediction accuracy of algorithm and percent accuracy
+    return pd.DataFrame({'Actual': ytest, 'Predicted': ypred}), mean_squared_error(ytest, ypred)
 
 
-xysplit = split(attributes, label)
-classifydf = treeClassify(xysplit[0], xysplit[1], xysplit[2], xysplit[3], plot=True)
+# starting function to drive other methods based on user choices
+def start():
+    # intro message
+    print("This program performs supervised machine learning predictive analysis on a chosen dataset.\n")
 
-regressiondf = treeRegression(xysplit[0], xysplit[1], xysplit[2], xysplit[3], plot=True)
+    print("What dataset would you like to use?\n"
+          "1. Select an existing dataset in the directory\n"
+          "2. Upload a new dataset\n")
 
-# print out accuracy of predicitons
-print("\nAccuracy of Predictions for Classifier Tree:")
-print(classifydf)
+    while True:
+        try:
+            ans = int(input("Choose an option above by entering its corresponding number: "))
+            if ans not in (1, 2):
+                raise ValueError
+        except ValueError:
+            print("Error: please enter the number 1 or 2.\n")
+        else:
+            break
 
-print("\nAccuracy of Predictions for Regression Tree:")
-print(regressiondf)
+    # choose file to use
+    if ans == 1:
+        chosenFile = existingFile()
+    elif ans == 2:
+        chosenFile = uploadFile()
+
+    # turn chosen .csv file into panda dataframe
+    data = initCSV(chosenFile)
+    # print dataset
+    print("Original Dataset:")
+    print(data)
+
+    # choose target attribute from dataset
+    target = targetCol(data)
+
+    # print(data.describe())
+
+    xysplit = split(data, target)
+
+    # choose operation
+    print("Would you like to perform tree classification or regression analysis on the dataset?\n"
+          "1. Classification\n"
+          "2. Regression\n")
+
+    while True:
+        try:
+            ans = int(input("Choose an option above by entering its corresponding number: "))
+            if ans not in (1, 2):
+                raise ValueError
+        except ValueError:
+            print("Error: please enter the number 1, 2, or 3.\n")
+        else:
+            break
+
+    if ans == 1:
+        classifydf = treeClassify(xysplit[0], xysplit[1], chosenFile, plot=True)
+        # print out accuracy of predictions, round percent accuracy to 2 decimals
+        print("\nAccuracy of Predictions for Classifier Tree: " + str(round(classifydf[1], 2)) + "%")
+        print(classifydf[0])
+    elif ans == 2:
+        regressiondf = treeRegression(xysplit[0], xysplit[1], chosenFile, plot=True)
+        print("\nRoot Mean Square Error for Regression Tree: " + str(round(regressiondf[1], 5)))
+        print(regressiondf[0])
+
+    # choose operation
+    print("What dimensionality reduction technique do you want to perform?\n"
+          "1. Missing Value Ratio\n"
+          "2. High Correlation Filter\n")
+
+    while True:
+        try:
+            ans = int(input("Choose an option above by entering its corresponding number: "))
+            if ans not in (1, 2):
+                raise ValueError
+        except ValueError:
+            print("Error: please enter the number 1, 2, or 3.\n")
+        else:
+            break
+
+    if ans == 1:
+        newData = missing(data)
+    elif ans == 2:
+        # remove high correlation and set as new dataframe
+        newData = correlation(data)
+
+    # check if changes were made
+    if newData.equals(data):
+        print("Data remains unchanged.")
+    else:
+        # option to save new data
+        while True:
+            try:
+                ans = input("Would you like to save your new dataset as a new .csv file?\nEnter 'y' or 'n': ")
+                if ans not in ('y', 'Y', 'n', 'N'):
+                    raise ValueError
+            except ValueError:
+                print("Error: please enter 'y' for yes or 'n' for no.\n")
+            else:
+                break
+
+        if ans in ('y', 'Y'):
+            # save to new .csv
+            save(newData)
+
+    while True:
+        try:
+            ans = input("Would you like to operate on another .csv file?\nEnter 'y' or 'n': ")
+            if ans not in ('y', 'Y', 'n', 'N'):
+                raise ValueError
+        except ValueError:
+            print("Error: please enter 'y' for yes or 'n' for no.\n")
+        else:
+            break
+
+    if ans in ('y', 'Y'):
+        # restart program
+        start()
+    elif ans in ('n', 'N'):
+        # end program
+        print("Bye!")
+        sys.exit()
+
+
+start()
